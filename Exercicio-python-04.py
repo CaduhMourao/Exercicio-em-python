dc4049ac8ef91dcf3cb768afcfb5191455a3b614
Caduh <caduh.dev@gmail.com>
2023-01-20 11:32:09 -0300

    Exercicio 4

---
+++ Exercicio-python-04.py
@@ -0,0 +1,10 @@
+a = input("Digite algo: ")
+
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É númerico?', a.isnumeric())
+print('È alfabético?', a.isalpha())
+print('É alfabético e númerico?', a.isalnum())
+print('Está em maiúsculo?', a.isupper())
+print('Está em minúsculo?', a.islower())
+print('Está capitalizada?', a.istitle())
