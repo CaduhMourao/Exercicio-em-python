28b670eeb0756fdf7caae426b37a75b8ee764a7d
Caduh <caduh.dev@gmail.com>
2023-01-24 10:35:05 -0300

    exercicio 11

---
+++ 11 - Pintando parede.py
@@ -0,0 +1,8 @@
+largura = float(input('Digite a largura: '))
+altura = float(input('Digite a altura: '))
+
+metros = largura * altura
+
+litros =  metros / 2
+
+print(f'Para pintar {metros}m² é necessário {litros}L de tinta.')
